c633f39ac1820ce9330419b1c0b417583ad8626b
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2018-07-10 13:10:24 +0700

    change config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'pitcher.freevar.com'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', '1109566'),
-            'username' => env('DB_USERNAME', '1109566'),
-            'password' => env('DB_PASSWORD', 'rahasia'),
+            'database' => env('DB_DATABASE', 'pitcher'),
+            'username' => env('DB_USERNAME', 'pitcher'),
+            'password' => env('DB_PASSWORD', 'rahasiasekali'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
